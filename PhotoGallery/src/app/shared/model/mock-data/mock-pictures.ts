232b577c7762b2330d2c5ce79e315238147533fa
Carmelo Bastillo <melonemark8@gmail.com>
2020-08-06 11:28:58 +0200

    add mock data

---
+++ PhotoGallery/src/app/shared/model/mock-data/mock-pictures.ts
@@ -0,0 +1,94 @@
+import { IPicture } from './../picture';
+
+export const PICTURES: IPicture [] = [
+{
+   id:1,
+   name:"La bellezza",
+   author:"Giuseppe Rossi",
+   like:4,
+   imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+   description:"La bellezza nel mondo",
+   status:false
+},
+{
+    id:2,
+    name:"La Morte",
+    author:"Giuseppe Bianchi",
+    like:2,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"La morte in natura",
+    status:true
+ },
+ {
+    id:3,
+    name:"Il Sorriso",
+    author:"Gianni Trenti",
+    like:6,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Uno squarcio",
+    status:false
+ },
+ {
+    id:4,
+    name:"La disperazione",
+    author:"Gianni Trenti",
+    like:6,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Stati umani",
+    status:true
+ },
+ {
+    id:5,
+    name:"La sorte",
+    author:"Vinz Persico",
+    like:1,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Cose che capitano",
+    status:true
+ },
+ {
+    id:6,
+    name:"Il vino",
+    author:"Abdul Saras",
+    like:10,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Bere responsabilmente",
+    status:false
+ },
+ {
+    id:7,
+    name:"La grappa",
+    author:"Abdul Saras",
+    like:5,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Bere molto responsabilmente",
+    status:false
+ },
+ {
+    id:8,
+    name:"Il bacio",
+    author:"Sarah Amore",
+    like:5,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Unione tra corpi",
+    status:true
+ },
+ {
+    id:9,
+    name:"Camurria",
+    author:"Mario Rossi",
+    like:8,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Ogni tanto capita",
+    status:true
+ },
+ {
+    id:10,
+    name:"Pasta al forno",
+    author:"Mario Bordò",
+    like:10,
+    imgPath:"https://cdn.pixabay.com/photo/2018/05/10/22/37/the-framework-3388998_960_720.jpg",
+    description:"Le cose belle della vita",
+    status:false
+ }
+]
