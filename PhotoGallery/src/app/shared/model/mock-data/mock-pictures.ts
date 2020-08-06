e71f711e83d5d12d019e874d38343833336fd198
Stefano Rizzo <stefanorizzo295@gmail.com>
2020-08-06 11:29:02 +0200

    Aggiunta interfaccia e dati mokkati

---
+++ PhotoGallery/src/app/shared/model/mock-data/mock-pictures.ts
@@ -0,0 +1,94 @@
+import { IPicture } from './../picture';
+
+export const PICTURES: IPicture[] = [
+  {
+    id: 1,
+    name: 'Gioconda',
+    author: 'Leonardo Da Vinci',
+    like: 5,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: true
+  },
+  {
+    id: 2,
+    name: 'Guernica',
+    author: 'Pablo Picasso',
+    like: 8,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: false
+  },
+  {
+    id: 3,
+    name: 'La Nascita di Venere',
+    author: 'Sandro Botticelli',
+    like: 4,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: true
+  },
+  {
+    id: 4,
+    name: 'Il Bacio',
+    author: 'Gustav Klimt',
+    like: 6,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: false
+  },
+  {
+    id: 5,
+    name: 'La Dama con L\' Ermellino',
+    author: 'Leonardo Da Vinci',
+    like: 3,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: false
+  },
+  {
+    id: 6,
+    name: 'Notte Stellata',
+    author: 'Vincent van Gogh',
+    like: 7,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: true
+  },
+  {
+    id: 7,
+    name: 'Gioconda',
+    author: 'Leonardo Da Vinci',
+    like: 5,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: false
+  },
+  {
+    id: 8,
+    name: 'Scudo con testa di medusa',
+    author: 'Caravaggio',
+    like: 10,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: true
+  },
+  {
+    id: 9,
+    name: 'Scuola di Atene',
+    author: 'Raffaello Sanzio',
+    like: 8,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: true
+  },
+  {
+    id: 10,
+    name: 'Primavera',
+    author: 'Sandro Botticelli',
+    like: 4,
+    imgPath: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non augue non purus fringilla finibus eget et lacus. Pellentesque dui massa, luctus eu tempor non, ultricies id tortor. Etiam congue ex arcu, et interdum mi varius at. Donec augue sem, mollis scelerisque velit viverra, vestibulum maximus sem. Aenean fringilla cursus ante et rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc vel tempus ex. Pellentesque vulputate dolor arcu, eget ultrices justo rutrum sed.',
+    status: false
+  }
+]
