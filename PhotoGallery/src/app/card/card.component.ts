444edca112f07712cd0baf133d47fb16e1450fe4
Paolo Tabacco <tabaccopaolo99@gmail.com>
2020-08-08 17:02:32 +0200

    commit componente card

---
+++ PhotoGallery/src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { AppModule } from './../app.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
